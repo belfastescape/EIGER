99fc2bdc0dd845323591f6d20ddee38026c99e88
belfastescape <stnic4412@gmail.com>
2026-04-01 21:55:55 +1300

    added sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://eigerescaperooms.com/sitemap.xml",
+    sitemap: "https://www.eigerescaperooms.com/sitemap.xml",
   }
 }
